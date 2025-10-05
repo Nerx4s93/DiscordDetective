4f5b011478b2d27e714afdf59223d73a41df186e
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-10-05 14:54:35 +0700

    Проверка дирректорий

---
+++ DataManager.cs
@@ -0,0 +1,18 @@
+﻿namespace DiscordDetective;
+
+internal static class DataManager
+{
+    public static void CheckDirectories()
+    {
+        CheckDirrectory("data");
+        CheckDirrectory(@"data\users");
+    }
+
+    private static void CheckDirrectory(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+}
