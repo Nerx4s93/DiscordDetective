d0af5c71323e6b157cb69b937812e6e7b5d729d7
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-11-28 19:14:45 +0700

    Указание токена

--- Database/Models/BotDTO.cs
+++ Database/Models/BotDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace DiscordDetective.Database.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiscordDetective.Database.Models;
 
 public class BotDTO
 {
+    [Key]
     public string Token { get; set; } = string.Empty;
-    public string Id { get; set; } = string.Empty;
+    public string? UserId { get; set; }
 }
