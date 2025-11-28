39534cc61e5075fd59cbb7c5794f1600e94e2322
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-11-28 18:52:47 +0700

    Удаление лишних namespace

--- Database/Models/BotDTO.cs
+++ Database/Models/BotDTO.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DiscordDetective.Database.Models;
+﻿namespace DiscordDetective.Database.Models;
 
 public class BotDTO
 {
