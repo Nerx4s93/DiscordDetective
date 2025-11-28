a89898e937f4af6f8defa0a22e4c5a8af9511223
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-11-28 16:09:31 +0700

    Метод конвертации

--- UserDTOExtensions.cs
+++ UserDTOExtensions.cs
@@ -23,4 +23,23 @@ public static class UserDTOExtensions
 
         return userDb;
     }
+
+    public static UserApiDTO ToApiDTO(this UserDbDTO user)
+    {
+        var userDb = new UserApiDTO()
+        {
+            Id = user.Id,
+            Username = user.Username,
+            GlobalName = user.GlobalName,
+            Discriminator = user.Discriminator,
+            Avatar = user.Avatar,
+            Banner = user.Banner,
+            AccentColor = user.AccentColor,
+            Bio = user.Bio,
+            Email = user.Email,
+            Verified = user.Verified,
+        };
+
+        return userDb;
+    }
 }
