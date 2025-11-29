7cec3e3f41374f52ccd24a09c20ae2935990dfa3
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-11-29 14:20:57 +0700

    Класс для управления картинками

---
+++ ImageDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace DiscordDetective;
+
+public class ImageDatabase
+{
+    private const string _path = "images";
+
+    public ImageDatabase()
+    {
+        Directory.CreateDirectory(_path);
+    }
+
+    public void Store(Image image, string fileName)
+    {
+        var path = Path.Combine(_path, fileName);
+        image.Save(path);
+    }
+
+    public Image? Load(string fileName)
+    {
+        var path = Path.Combine(_path, fileName);
+        if (!File.Exists(path))
+        {
+            return null;
+        }    
+
+        var image = Image.FromFile(path);
+        return image;
+    }
+}
