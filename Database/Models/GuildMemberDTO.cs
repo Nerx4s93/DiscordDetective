5f10018c8592bb5632bd3bdea3e7fc1235e671d7
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-11-30 13:51:20 +0700

    Класс для связи участиников сервера и серверов

---
+++ Database/Models/GuildMemberDTO.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DiscordDetective.Database.Models;
+
+[PrimaryKey(nameof(UserId), nameof(GuildId))]
+public class GuildMemberDTO
+{
+    public string UserId { get; set; } = string.Empty;
+
+    public string GuildId { get; set; } = string.Empty;
+}
