b0391535eb6fc3e787a7ed6cc3a69690e404cbc7
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-12-01 22:49:42 +0700

    Базовая загрузка

--- GUI/FormBot.Designer.cs
+++ GUI/FormBot.Designer.cs
@@ -28,11 +28,59 @@ partial class FormBot
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "FormBot";
+        tabControl1 = new System.Windows.Forms.TabControl();
+        tabPage1 = new System.Windows.Forms.TabPage();
+        tabPage2 = new System.Windows.Forms.TabPage();
+        tabControl1.SuspendLayout();
+        SuspendLayout();
+        // 
+        // tabControl1
+        // 
+        tabControl1.Controls.Add(tabPage1);
+        tabControl1.Controls.Add(tabPage2);
+        tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+        tabControl1.Location = new System.Drawing.Point(0, 0);
+        tabControl1.Multiline = true;
+        tabControl1.Name = "tabControl1";
+        tabControl1.SelectedIndex = 0;
+        tabControl1.Size = new System.Drawing.Size(800, 450);
+        tabControl1.TabIndex = 0;
+        // 
+        // tabPage1
+        // 
+        tabPage1.Location = new System.Drawing.Point(4, 34);
+        tabPage1.Name = "tabPage1";
+        tabPage1.Padding = new System.Windows.Forms.Padding(3);
+        tabPage1.Size = new System.Drawing.Size(792, 412);
+        tabPage1.TabIndex = 0;
+        tabPage1.Text = "Информация бота";
+        tabPage1.UseVisualStyleBackColor = true;
+        // 
+        // tabPage2
+        // 
+        tabPage2.Location = new System.Drawing.Point(4, 34);
+        tabPage2.Name = "tabPage2";
+        tabPage2.Padding = new System.Windows.Forms.Padding(3);
+        tabPage2.Size = new System.Drawing.Size(792, 412);
+        tabPage2.TabIndex = 1;
+        tabPage2.Text = "Сервера";
+        tabPage2.UseVisualStyleBackColor = true;
+        // 
+        // FormBot
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(tabControl1);
+        Name = "FormBot";
+        Text = "FormBot";
+        tabControl1.ResumeLayout(false);
+        ResumeLayout(false);
     }
 
     #endregion
+
+    private System.Windows.Forms.TabControl tabControl1;
+    private System.Windows.Forms.TabPage tabPage1;
+    private System.Windows.Forms.TabPage tabPage2;
 }
--- GUI/FormBot.cs
+++ GUI/FormBot.cs
@@ -1,14 +1,31 @@
-﻿using System.Windows.Forms;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace DiscordDetective.GUI;
 
 public partial class FormBot : Form
 {
+    private readonly DatabaseContext _databaseContext;
     private readonly string _token;
 
-    public FormBot(string token)
+    public FormBot(DatabaseContext databaseContext, string token)
     {
         InitializeComponent();
+        _databaseContext = databaseContext;
         _token = token;
+        _ = LoadBotsAsync();
     }
+
+    #region Загрузка
+
+    private async Task LoadBotsAsync()
+    {
+        var botDbDTO = _databaseContext.Bots.AsNoTracking().First(b => b.Token == _token);
+        var userDbDTO = _databaseContext.Users.AsNoTracking().Where(u => u.Id == botDbDTO.UserId);
+    }
+
+    #endregion
 }
