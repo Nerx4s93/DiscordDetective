8465ae6d7084ea876bf5c2040d604986fc5a4dd8
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-12-08 19:56:13 +0700

    Класс для работы с Svg

---
+++ UI/SvgRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Text;
+
+using Svg;
+
+namespace DiscordDetective.UI;
+
+internal class SvgRenderer
+{
+    public static Bitmap SvgToBitmap(string svgContent, int width, int height)
+    {
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(svgContent));
+        var svgDocument = SvgDocument.Open<SvgDocument>(stream);
+
+        var bitmap = new Bitmap(width, height);
+        using (var graphics = Graphics.FromImage(bitmap))
+        {
+            graphics.Clear(Color.Transparent);
+            svgDocument.Draw(graphics);
+        }
+
+        return bitmap;
+    }
+}
