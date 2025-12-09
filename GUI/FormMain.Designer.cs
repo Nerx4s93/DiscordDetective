d0481042e9028e15db5414a238022bcf6fbce7ca
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-12-09 15:51:47 +0700

    fix

--- GUI/FormMain.Designer.cs
+++ GUI/FormMain.Designer.cs
@@ -31,6 +31,7 @@ partial class FormMain
         components = new System.ComponentModel.Container();
         tabControlMain = new System.Windows.Forms.TabControl();
         tabPageProxy = new System.Windows.Forms.TabPage();
+        listView1 = new System.Windows.Forms.ListView();
         TabPageBots = new System.Windows.Forms.TabPage();
         listViewBots = new System.Windows.Forms.ListView();
         ContextMenuStripBots = new System.Windows.Forms.ContextMenuStrip(components);
@@ -38,7 +39,6 @@ partial class FormMain
         UpdateListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
         UpdateBotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
         DeleteBotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-        listView1 = new System.Windows.Forms.ListView();
         tabControlMain.SuspendLayout();
         tabPageProxy.SuspendLayout();
         TabPageBots.SuspendLayout();
@@ -53,7 +53,7 @@ partial class FormMain
         tabControlMain.Location = new System.Drawing.Point(0, 0);
         tabControlMain.Name = "tabControlMain";
         tabControlMain.SelectedIndex = 0;
-        tabControlMain.Size = new System.Drawing.Size(1218, 726);
+        tabControlMain.Size = new System.Drawing.Size(1308, 726);
         tabControlMain.TabIndex = 0;
         tabControlMain.TabStop = false;
         // 
@@ -62,10 +62,18 @@ partial class FormMain
         tabPageProxy.Controls.Add(listView1);
         tabPageProxy.Location = new System.Drawing.Point(4, 34);
         tabPageProxy.Name = "tabPageProxy";
-        tabPageProxy.Size = new System.Drawing.Size(1210, 688);
+        tabPageProxy.Size = new System.Drawing.Size(1300, 688);
         tabPageProxy.TabIndex = 1;
         tabPageProxy.Text = "Прокси";
         tabPageProxy.UseVisualStyleBackColor = true;
+        // 
+        // listView1
+        // 
+        listView1.Location = new System.Drawing.Point(0, 0);
+        listView1.Name = "listView1";
+        listView1.Size = new System.Drawing.Size(1300, 688);
+        listView1.TabIndex = 0;
+        listView1.UseCompatibleStateImageBehavior = false;
         // 
         // TabPageBots
         // 
@@ -73,7 +81,7 @@ partial class FormMain
         TabPageBots.Location = new System.Drawing.Point(4, 34);
         TabPageBots.Name = "TabPageBots";
         TabPageBots.Padding = new System.Windows.Forms.Padding(3);
-        TabPageBots.Size = new System.Drawing.Size(1210, 688);
+        TabPageBots.Size = new System.Drawing.Size(1300, 688);
         TabPageBots.TabIndex = 0;
         TabPageBots.Text = "Боты";
         TabPageBots.UseVisualStyleBackColor = true;
@@ -84,7 +92,7 @@ partial class FormMain
         listViewBots.Dock = System.Windows.Forms.DockStyle.Fill;
         listViewBots.Location = new System.Drawing.Point(3, 3);
         listViewBots.Name = "listViewBots";
-        listViewBots.Size = new System.Drawing.Size(1204, 682);
+        listViewBots.Size = new System.Drawing.Size(1294, 682);
         listViewBots.TabIndex = 0;
         listViewBots.UseCompatibleStateImageBehavior = false;
         listViewBots.SelectedIndexChanged += listViewBots_SelectedIndexChanged;
@@ -127,20 +135,11 @@ partial class FormMain
         DeleteBotToolStripMenuItem.Text = "Удалить";
         DeleteBotToolStripMenuItem.Click += DeleteBotToolStripMenuItem_Click;
         // 
-        // listView1
-        // 
-        listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-        listView1.Location = new System.Drawing.Point(0, 0);
-        listView1.Name = "listView1";
-        listView1.Size = new System.Drawing.Size(1210, 688);
-        listView1.TabIndex = 0;
-        listView1.UseCompatibleStateImageBehavior = false;
-        // 
         // FormMain
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        ClientSize = new System.Drawing.Size(1218, 726);
+        ClientSize = new System.Drawing.Size(1308, 726);
         Controls.Add(tabControlMain);
         Name = "FormMain";
         Text = "FormMain";
