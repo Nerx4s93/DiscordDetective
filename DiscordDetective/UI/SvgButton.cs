818e1405a820291abbc9b80b947435eb3be70d7b
Nerx <102707294+Nerx4s93@users.noreply.github.com>
2025-12-25 20:51:14 +0700

    Доработка кнопки для отрисовки SVG

--- DiscordDetective/UI/SvgButton.cs
+++ DiscordDetective/UI/SvgButton.cs
@@ -2,41 +2,67 @@
 using System.Windows.Forms;
 
 using DiscordDetective.UI.Tools;
+using System.ComponentModel;
+using System.Net.Mime;
 
 namespace DiscordDetective.UI;
 
 public class SvgButton : Button
 {
-    public SvgButton()
+    private string _iconName;
+    private int _padding;
+
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    public string IconName
     {
-        base.Text = string.Empty;
-        TextImageRelation = TextImageRelation.Overlay;
+        get => _iconName;
+        set
+        {
+            _iconName = value;
+            Invalidate();
+        }
+    }
+
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    public new int Padding
+    {
+        get => _padding;
+        set
+        {
+            _padding = value;
+            Invalidate();
+        }
     }
 
     protected override void OnPaint(PaintEventArgs e)
     {
-        var originalText = base.Text;
         base.Text = string.Empty;
 
-        base.OnPaint(e);
+        try
+        {
+            base.OnPaint(e);
 
-        base.Text = originalText;
-        DrawSvgImage(e.Graphics, 5, originalText);
+            if (!string.IsNullOrEmpty(IconName))
+            {
+                DrawSvgImage(e.Graphics);
+            }
+        }
+        catch { /* ignore */ }
     }
 
-    private void DrawSvgImage(Graphics graphics, int padding, string svgPath)
+    private void DrawSvgImage(Graphics graphics)
     {
-        var svgCode = SvgDataLoader.GetSvgData($"Icon.{svgPath}");
+        var svgCode = SvgDataLoader.GetSvgData($"Icon.{IconName}");
 
         if (svgCode == null)
         {
             return;
         }
-        
-        var width = Width - padding * 2;
-        var height = Height - padding * 2;
+
+        var width = Width - _padding * 2;
+        var height = Height - _padding * 2;
         using var svgImage = SvgRenderer.SvgToBitmap(svgCode, width, height);
 
-        graphics.DrawImage(svgImage, padding, padding);
+        graphics.DrawImage(svgImage, _padding, _padding);
     }
 }
