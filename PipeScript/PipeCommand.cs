46eafb37ad0d46f42f6bc178d7f0452bb42ddb76
Nerx <102707294+Nerx4s93@users.noreply.github.com>
2025-12-26 16:16:35 +0700

    Передача контекста в команду

--- PipeScript/PipeCommand.cs
+++ PipeScript/PipeCommand.cs
@@ -6,15 +6,12 @@ public abstract class PipeCommand
 {
     public abstract string Name { get; }
 
-    public abstract object Execute(string[] args, Variables variables);
+    public abstract object Execute(string[] args, ExecutionContext ctx);
 
-    public virtual Task<object> ExecuteAsync(string[] args, Variables variables)
+    public virtual Task<object> ExecuteAsync(string[] args, ExecutionContext ctx)
     {
-        return Task.FromResult(Execute(args, variables));
+        return Task.FromResult(Execute(args, ctx));
     }
 
-    public virtual bool ValidateArgs(string[] args)
-    {
-        return true;
-    }
+    public virtual bool ValidateArgs(string[] args) => true;
 }
