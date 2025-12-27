bfe5460f763258171af063e1420966d15b5eebbb
Nerx <102707294+Nerx4s93@users.noreply.github.com>
2025-12-27 22:51:52 +0700

    Команда для вывода текста

---
+++ PipeScript/Commands/Echo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using PipeScript.CommandResults;
+
+namespace PipeScript.Commands;
+
+internal sealed class Echo : PipeCommand
+{
+    public override string Name { get; } = "echo";
+
+    public override ContinueResult Execute(string[] args, ExecutionContext ctx)
+    {
+        if (args.Length != 1)
+        {
+            throw new ArgumentException("Usage: echo <text>");
+        }
+
+        var arg = args[0].Trim();
+        var text = ScriptUtils.ResolveArg(arg, ctx.Variables).ToString();
+        ctx.Host.WriteLine(text!);
+
+        return ContinueResult.Instance;
+    }
+
+    public override bool ValidateArgs(string[] args)
+    {
+        return args.Length == 1;
+    }
+}
