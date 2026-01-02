0be1a3662dd2a6c367a7df6d0dbb5f7fd44d641a
Nerx <102707294+Nerx4s93@users.noreply.github.com>
2026-01-02 14:33:41 +0700

    Возможность получить доступ к внутренним переменным переменных

--- PipeScript/ScriptUtils.cs
+++ PipeScript/ScriptUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace PipeScript;
 
@@ -6,16 +7,37 @@ internal static class ScriptUtils
 {
     public static object ResolveArg(string arg, Variables vars)
     {
-        if (arg.StartsWith('$'))
+        if (!arg.StartsWith('$'))
         {
-            var varName = arg[1..];
-            var variable = vars.Get(varName);
-            if (variable == null)
-            {
-                throw new Exception($"Variable '{varName}' not found");
-            }
-            return variable.Value;
+            return arg;
         }
-        return arg;
+
+        var path = arg[1..].Split('.');
+
+        var variable = vars.Get(path[0]);
+        if (variable == null)
+        {
+            throw new Exception($"Variable '{path[0]}' not found");
+        }
+
+        object currentValue = variable.Value;
+        for (int i = 1; i < path.Length; i++)
+        {
+            if (currentValue == null)
+            {
+                throw new Exception($"Cannot access '{path[i]}' of null in '{arg}'");
+            }
+
+            var type = currentValue.GetType();
+            var prop = type.GetProperty(path[i], BindingFlags.Public | BindingFlags.Instance);
+            if (prop == null)
+            {
+                throw new Exception($"Property '{path[i]}' not found in type '{type.Name}' for '{arg}'");
+            }
+
+            currentValue = prop.GetValue(currentValue)!;
+        }
+
+        return currentValue;
     }
 }
